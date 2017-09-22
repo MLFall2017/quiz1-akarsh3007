6462b872a3bb62cf462bc7e444eaa449879b62b6
akarsh3007 <gupta.akarsh@gmail.com>
2017-09-22 10:47:03 -0400

    added plot pca

--- pca.py
+++ pca.py
@@ -1,6 +1,6 @@
 
 import numpy as np
-
+import matplotlib.pyplot as plt
 def PCA(input) :
 
     cov_in = np.corrcoef(input.T)
@@ -11,6 +11,10 @@ def PCA(input) :
     tot = sum(eig_val)
     eigval_per = [(i/tot)*100 for i in sorted(eig_val,reverse=True)]
     cum_sum = np.cumsum(eigval_per)
+    range_x = np.array([1,50,100])
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(range_x,cum_sum)
     matrix_w = np.hstack((eig_pairs[0][1].reshape(3,1),eig_pairs[1][1].reshape(3,1)))
     reduced = input.dot(matrix_w)
     return reduced
